3d6616994ba0f99a03cd65448fa4f79c137b2e8f
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-09-09 14:20:39 +0300

    firebase_nuxtjs_crud_App

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,17 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "firebase_crud_app",
+    title: "firebase",
     htmlAttrs: {
       lang: "en",
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      { hid: "description", name: "description", content: "firebase" },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/firebase.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
